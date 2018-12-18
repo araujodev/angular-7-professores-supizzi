aff6c6d16e68b9c3a4330b6cdfbf3196924b77b9
LEANDRO ARAUJO <laraujo@corp.geoi2.com.br>
2018-12-18 12:13:15 -0300

    Iniciado Authentication e ajustado layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatToolbarModule } from "@angular/material/toolbar";
 
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { fakeBackendProvider } from "./_helpers";
+import { JwtInterceptor, ErrorInterceptor } from "./_helpers";
+
+import { HomeComponent } from "./home";
+import { LoginComponent } from "./login";
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
