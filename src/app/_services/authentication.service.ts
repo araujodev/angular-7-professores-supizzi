f3094891f55debc1e9b24fdccf4c1e084c8b757a
LEANDRO ARAUJO <laraujo@corp.geoi2.com.br>
2018-12-18 13:30:41 -0300

    ajustes em test api com https://reqres.in/

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -23,20 +23,28 @@ export class AuthenticationService {
 
   login(username: string, password: string) {
     return this.http
-      .post<any>(`http://localhost:4200/users/authenticate`, {
+      .post<any>(`https://reqres.in/api/login`, {
         username,
         password
       })
       .pipe(
         map(user => {
-          // login successful if there's a jwt token in the response
+          let userObject = {
+            id: 1,
+            username: "teste",
+            firstName: "first",
+            lastName: "last",
+            password: "",
+            token: ""
+          };
+
           if (user && user.token) {
-            // store user details and jwt token in local storage to keep user logged in between page refreshes
-            localStorage.setItem("currentUser", JSON.stringify(user));
-            this.currentUserSubject.next(user);
+            userObject.token = user.token;
+            localStorage.setItem("currentUser", JSON.stringify(userObject));
+            this.currentUserSubject.next(userObject);
           }
 
-          return user;
+          return userObject;
         })
       );
   }
